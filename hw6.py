030c49129cbe1855adbdff0207f5381162db6452
jjslovelego <jjslovelego@gmail.com>
2025-04-25 15:35:43 +0900

    Add files via upload

---
+++ hw6.py
@@ -0,0 +1,13 @@
+
+def display_multiplication_table():
+    for i in range(1,10):
+        print(f'2*{i}={2*i:2d}')
+        print(f'3*{i}={3*i:2d}')
+        print(f'4*{i}={4*i:2d}')
+        print(f'5*{i}={5*i:2d}')
+        print(f'6*{i}={6*i:2d}')
+        print(f'7*{i}={7*i:2d}')
+        print(f'8*{i}={8*i:2d}')
+        print(f'9*{i}={9*i:2d}')
+        
+display_multiplication_table()
